3d4d1b70cf3ed6175efac3a07215f5217dbadc05
Maryna <52110829+zolottareva@users.noreply.github.com>
2021-01-08 12:43:25 +0200

    finished 8th task
    better learn how to read bold text👿

---
+++ Leetcode January Challange/leetcode_january8/leetcode_january8/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  leetcode_january8
+//
+//  Created by Золотарева Марина Олеговна on 08.01.2021.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using std::string;
+using std::vector;
+
+
+bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+    string first = "";
+    string second = "";
+    
+    size_t element1 = 0, element2 = 0;
+    size_t sizeOf1 = word1.size(), sizeOf2 = word2.size();
+    
+    while (element1 < sizeOf1 || element2 < sizeOf2){
+        
+    }
+    return first == second;
+}
+
+int main() {
+    
+    return 0;
+}
